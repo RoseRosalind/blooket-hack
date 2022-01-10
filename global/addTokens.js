c682bc388609ff845d7d787fbb6d93dec961a446
RoseRosalind <97435819+RoseRosalind@users.noreply.github.com>
2022-01-10 15:47:06 +0700

    addTokens
    I want more coins

--- global/addTokens.js
+++ global/addTokens.js
@@ -15,7 +15,7 @@ async function getName() {
 async function addCurrencies() {
     const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
 
-    if (tokens > 500) {
+    if (tokens > 50000000) {
         alert('You can only add up to 500 tokens daily.');
     };
 
@@ -28,7 +28,7 @@ async function addCurrencies() {
         credentials: "include",
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 300000,
             name: await getName()
         })
     });
